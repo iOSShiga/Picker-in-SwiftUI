4c352bf0775c82613b5e8389032317f52c38e410
Shigasuresh@123 <shigasuresh546@gmail.com>
2020-02-26 22:14:38 +0530

    Initial Commit

---
+++ SimplePickerDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimplePickerDemo
+//
+//  Created by shiga on 26/02/20.
+//  Copyright © 2020 shiga. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
