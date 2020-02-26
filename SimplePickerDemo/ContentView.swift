3ac0571f7ab42d55d4a933d7343dc710ae412e62
Shigasuresh@123 <shigasuresh546@gmail.com>
2020-02-26 22:32:16 +0530

    initial

--- SimplePickerDemo/ContentView.swift
+++ SimplePickerDemo/ContentView.swift
@@ -9,8 +9,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var animals  = ["Kangaroo", "Monkey", "Snake", "Zebra", "Panda", "Hippo","Bull","Elephant","Tiger","Gorilla","Eagle","Gorilla","Lion","Giraffe"]
+    
+   @State private var selectedIndex = 0
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Form {
+            Section {
+            Picker(selection: $selectedIndex, label: Text("Favourite Animal")) {
+                ForEach(0 ..< animals.count) {
+                    Text(self.animals[$0])
+                }
+            }
+            }
+            }
+            .navigationBarTitle(Text("Wild Animals"))
+    }
     }
 }
 
